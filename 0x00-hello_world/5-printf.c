989c6daa69b1472b8f9372b5bd6d37632130ad21
Jesutoby <adetoyane@gmail.com>
2022-10-13 11:48:12 -0700

    mandatory task

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main function is entry point for the program
+ * main - function is entry point for the program
  * prints with proper grammar, but the outcome is a piece of art,
  * using the printf function
  * Return 0 if exited properly
@@ -9,5 +9,5 @@
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return(0);
+	return (0);
 }
