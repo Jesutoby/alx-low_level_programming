6c16ecf1d42a9374bcd4c7503d5ace1aa924af13
Jesutoby <adetoyane@gmail.com>
2022-10-13 11:37:02 -0700

    mandatory task

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main function is entry point for the program
+ * prints "Programming is like building a multilingual puzzle
+ * using the puts function
+ * Return 0 if exited properly
+ * 
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main function is entry point for the program
+ * prints with proper grammar, but the outcome is a piece of art,
+ * using the printf function
+ * Return 0 if exited properly
+ *
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return(0);
+}
