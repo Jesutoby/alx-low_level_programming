fa7392d6263059bc74bef04bbf83448522eb6c90
Enoch Adetoyan <67147518+Jesutoby@users.noreply.github.com>
2022-10-16 17:07:01 +0100

    Task 5

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -7,13 +7,10 @@
  */
 int main(void)
 {
-	int n = 0;
+	int n;
 
-	while (n < 10)
-        {
+	for (n = 0; n < 10; n++)
 		printf("%d", n);
-                n++
-        }
 	printf("\n");
 	return (0);
 }
