a0c0b85841404da0522aad520248f10546660505
Jesutoby <adetoyane@gmail.com>
2022-10-18 16:04:07 -0700

    Task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_last_digit - returns the last digit of a number,
+ * by getting the remainder on division by 10.
+ * @a: is the number whose last digit is to be returned.
+ *
+ * Return: the last digit e.g a = 203 , it returns 3
+ */
+
+int print_last_digit(int a)
+{
+	return (a % 10);
+}
