19ac0c195ef3f1c9184cee5e69426c180e596506
Enoch Adetoyan <67147518+Jesutoby@users.noreply.github.com>
2022-10-18 18:55:03 +0100

    Created _putchar.c

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,11 @@
+#include <unistd.h>
+/**
+* _putchar - writes to standard output (denoted by 1),
+* @c is what is written to standard output,
+* Return: the byte of c (a char), which is 1
+*/
+
+int _putchar(char c)
+{
+        return(write(1, &c, 1));
+}
