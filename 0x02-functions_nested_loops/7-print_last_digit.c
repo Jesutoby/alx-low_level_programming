a92eaac4dd4b6287cac5935a7b67af9bbcd1dade
Jesutoby <adetoyane@gmail.com>
2022-10-18 16:20:29 -0700

    Task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,10 +9,17 @@
  */
 
 int print_last_digit(int x)
-{	
+{
+	int y;
+
 	if (x < 0)
 		x = -x;
 
-	_putchar((x % 10) + '0');
-	return (x % 10);
+	y = x % 10;
+
+	if (y < 0)
+		y = -y;
+
+	_putchar(y + '0');
+	return (y);
 }
