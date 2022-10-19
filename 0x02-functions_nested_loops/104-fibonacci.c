f4d49712c24f99eb6b84d8feba34dcdb8144b4f9
Enoch Adetoyan <67147518+Jesutoby@users.noreply.github.com>
2022-10-19 07:46:16 +0100

    Advanced task

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,15 +8,15 @@
  */
 int main(void)
 {
-	long int i, j, k, j1, j2, k1, k2;
+	unsigned long int i, j, k, j1, j2, k1, k2;
 
 	j = 1;
 	k = 2;
 
-	printf("%ld", j);
+	printf("%lu", j);
 	for (i = 1; i < 91; i++)
 	{
-		printf(", %ld", k);
+		printf(", %lu", k);
 		k = k + j;
 		j = k - j;
 	}
@@ -27,8 +27,8 @@ int main(void)
 
 	for (i = 92; i < 99; ++i)
 	{
-		printf(", %ld", k1 + (k2 / 1000000000));
-		printf("%ld", k2 % 1000000000);
+		printf(", %lu", k1 + (k2 / 1000000000));
+		printf("%lu", k2 % 1000000000);
 		k1 = k1 + j1;
 		j1 = k1 - j1;
 		k2 = k2 + j2;
