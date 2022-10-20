6081c761e98fed021b796d1902d25ddaf42fe555
Jesutoby <adetoyane@gmail.com>
2022-10-20 14:45:37 -0700

    task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* _isupper - checks if int c is a lowercase letter,
+* in ASCII.
+* @c: is the variable name for the character.
+* Return: 1 if c is lowercase, return 0 otherwise
+*/
+
+int _isupper(int c)
+{
+	return (c >= 65 && c <= 90);
+}
