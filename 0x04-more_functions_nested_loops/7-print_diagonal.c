c0e9a577c46f7c7be1b07f88e42829df8fd50cd3
Jesutoby <adetoyane@gmail.com>
2022-10-20 17:23:45 -0700

    task 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* print_diagonal - prints \ diagonally,
+* followed by a new line.
+* @n: is the parameter for the number of times to print.
+*/
+
+void print_diagonal(int n)
+{
+	int a, b;
+
+	for (a = 1; a <= n; a++)
+	{
+		for (b = 1; b <= n; b++)
+		{
+			if (b < a)
+				_putchar(' ');
+			else if (b == a)
+				_putchar('\\');
+		}
+		_putchar('\n');
+	}
+}
