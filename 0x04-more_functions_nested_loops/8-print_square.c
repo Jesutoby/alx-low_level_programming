81072affd14c32f8152014e9092c80ae37ea84cb
Jesutoby <adetoyane@gmail.com>
2022-10-20 18:07:58 -0700

    task 8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+* print_square - prints square with #,
+* having same number of row(s) and column(s).
+* @size: is the parameter for the size of square.
+*/
+
+void print_square(int size)
+{
+	int a, b;
+
+	if (size <= 0)
+		_putchar('\n');
+
+	for (a = 1, a <= size; a++)
+	{
+		for (b = 1; b <= size; b++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
